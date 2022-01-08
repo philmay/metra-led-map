df6d769ce9bf6d0594f6772719337d2a3c2afadb
philmay <phil.may@motorolasolutions.com>
2022-01-08 17:40:08 -0600

    Using push_to_driver rather than update for LED function, and removed som debug logginge

--- http-led-server.py
+++ http-led-server.py
@@ -30,7 +30,7 @@ class MyHandler(BaseHTTPRequestHandler):
         self.wfile.write(bytearray(str(leds_state), 'utf-8'))
 
     def do_POST(self):
-        print("Received POST request:", str(self))
+        print("Received POST request.")
         global leds_state
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
@@ -39,9 +39,10 @@ class MyHandler(BaseHTTPRequestHandler):
             code = 201
             statusMessage = '{"status":"ok"}'
             body_array = json.loads(post_data.decode('utf-8'))
-            print("Parsed JSON -> python object:", str(body_array), "\n")
+            # print("Parsed JSON -> python object:", str(body_array), "\n")
             led.all_off()
-            led.update()
+            # led.update()
+            led.push_to_driver()
             leds_state = []
 
             led.set_brightness(64)
@@ -55,13 +56,14 @@ class MyHandler(BaseHTTPRequestHandler):
                         led_object['color'] = 'White'
 
                     leds_state.append(led_object)
-                    print("    lighting " + str(led_object['position']) + " " + led_object['color'])
+                    # print("    lighting " + str(led_object['position']) + " " + led_object['color'])
                     led.set(led_object['position'], led_color)
 
                 else:
                     print("\tERROR: No 'position' attribute in object")
             print("    updating led module")
-            led.update()
+            # led.update()
+            led.push_to_driver()
 
         else:
             print("\tERROR: Content-Type not application/json - ", self.headers['Content-Type'])
@@ -74,7 +76,8 @@ class MyHandler(BaseHTTPRequestHandler):
     def do_DELETE(self):
         global leds_state
         led.all_off()
-        led.update()
+        # led.update()
+        led.push_to_driver()
         leds_state = []
         self._set_response(204)
         self.wfile.write(bytearray('{"status":"cleared"}', 'utf-8'))
@@ -92,7 +95,8 @@ def run(server_class=HTTPServer, handler_class=MyHandler, port=8675):
     print('Stopping httpd...\n')
     print('Clearing LEDs...\n')
     led.all_off()
-    led.update()
+    # led.update()
+    led.push_to_driver()
 
 if __name__ == '__main__':
     from sys import argv
